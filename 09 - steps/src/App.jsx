92a6eec8cba17e9185e2df29356f731210486692
ziadseleem <ziad32seleem@gmail.com>
2026-03-21 14:31:27 +0200

    implement steps component with navigation and update styles

--- 09 - steps/src/App.jsx
+++ 09 - steps/src/App.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  const [step, setStep] = useState(1);
+
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
+
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  }
+
+  return (<>
+    <div className="steps">
+      <div className="numbers">
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
+      </div>
+      <p className="message">{messages[step - 1]}</p>
+      <div className="buttons">
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+    </div>
+  </>)
 }
 
 export default App
